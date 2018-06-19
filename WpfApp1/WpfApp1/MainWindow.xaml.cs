dd8cf56137f782d5809b7502895ef173020dc778
VladyslavSH <manujlenkovlad2@gmail.com>
2018-06-19 12:49:31 +0300

    Work is changed

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,32 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        string path = null, file = null;
+        string [] nameFile = null;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog Ofd = new OpenFileDialog();
+            if (Ofd.ShowDialog() == true)
+            {
+                path = Ofd.FileName;
+                nameFile = Ofd.FileNames;
+            }
+            //Ofd.Filter = "Image files(*.png)|*.png| Image files(*.jpg)|*.jpg| Image files(*.jpeg)|*.jpeg";
+            Console.WriteLine(path);
+            using(FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                using(StreamReader sr = new StreamReader(fs, Encoding.Default))
+                {
+                    file = sr.ReadToEnd();
+                }
+            }
+            imageList.ItemsSource = file;
+            FileName.Text = path;
         }
     }
 }
